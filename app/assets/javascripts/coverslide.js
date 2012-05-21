d78222a5e85c60c47752c079be09bd935e0ae46e
Benton <bentonrochester@gmail.com>
2012-05-21 15:35:26 -0600

    mobile script

--- app/assets/javascripts/coverslide.js
+++ app/assets/javascripts/coverslide.js
@@ -22,14 +22,3 @@ var $window = $(window);
 	
 
 			
-if( navigator.userAgent.match(/Android/i)
- || navigator.userAgent.match(/webOS/i)
- || navigator.userAgent.match(/iPhone/i)
- || navigator.userAgent.match(/iPad/i)
- || navigator.userAgent.match(/iPod/i)
- || navigator.userAgent.match(/BlackBerry/i)
- ){
-$('#cover').addClass("alt");
-				$('#cover img').addClass("alt");
-				$('nav').addClass("still");		
-};
