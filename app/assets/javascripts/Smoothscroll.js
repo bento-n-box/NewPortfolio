8015d3a57152a496e7dc4d59b3c6b22dca4bd0de
Benton <bentonrochester@gmail.com>
2012-06-07 10:28:22 -0600

    -A

--- app/assets/javascripts/Smoothscroll.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-  $(".scroll").click(function(event){
-    event.preventDefault();
-    var offset = $($(this).attr('href')).offset().top;
-    $('html, body').animate({scrollTop:offset}, 500);
-  });
-});
