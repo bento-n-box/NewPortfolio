e53172917cb65b8e02f98d22883232d46c49702b
Benton <bentonrochester@gmail.com>
2012-06-11 15:35:09 -0600

    mailer update

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -8,7 +8,7 @@ class ContactController < ApplicationController
     @message = Message.new(params[:message])
     
     if @message.valid?
-      Notifications_Mailer.new_message(@message).deliver
+      notifications_mailer.new_message(@message).deliver
       redirect_to(root_path, :notice => "Message was successfully sent.")
     else
       flash.now.alert = "Please fill all fields."
