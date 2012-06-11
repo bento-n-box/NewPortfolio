00d2bb6e149ebee246a2870b10c58984997973df
Benton <bentonrochester@gmail.com>
2012-06-11 15:45:00 -0600

    mailer update

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ NewPortfolio::Application.routes.draw do
 	root :to => 'contact#new'
 	get "pages/index"
     	 
- 	resources :posts 
+ 	resources :contact_controller
   	#match 'contact' => 'contact#new', :as => 'contact', :via => :get
 	match 'contact' => 'contact#thanks.html', :as => 'contact', :via => :post
   # The priority is based upon order of creation:
