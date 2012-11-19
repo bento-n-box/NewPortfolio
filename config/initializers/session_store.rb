0b89fc34d640f6c60181aef3915648fb9558834f
rochestb <bentonrochester@gmail.com>
2012-11-19 10:17:07 -0700

    last commit on work comp

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -7,3 +7,4 @@ NewPortfolio::Application.config.session_store :cookie_store
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
 # NewPortfolio::Application.config.session_store :active_record_store
+ 
